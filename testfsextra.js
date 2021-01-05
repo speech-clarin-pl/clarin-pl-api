9088b24e601ad44ff75761b0f400f0dd1b7df314
Mariusz <mklec@pjwstk.edu.pl>
2021-01-05 10:43:46 +0100

    uprządkowano gitignore oraz wywolanie aplikacji z NODE_ENV

--- testfsextra.js
+++
@@ -1,5 +0,0 @@
-const fs = require('fs-extra');
-
-fs.copy('./jakisprosty.txt', './drugiplikskopiowany.txt')
-  .then(() => console.log('success!'))
-  .catch(err => console.error("wystapil blad" + err));
